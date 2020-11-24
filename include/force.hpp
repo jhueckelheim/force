ed7c2ccc5c26cabc0c5d3ee8b0eac0394c90cf07
Jan Hueckelheim <j.hueckelheim@imperial.ac.uk>
2020-11-24 13:36:39 -0600

    improved const-correctness, added sqrt and assignment operators

--- include/force.hpp
+++ include/force.hpp
@@ -1,5 +1,9 @@
+#ifndef force_hpp
+#define force_hpp
+
 #include <iostream>
 #include <math.h>
+#include "mpfrcpp_tpl.h"
 
 template<typename T>
 class freal{
@@ -32,14 +36,20 @@ class freal{
 
     static float multiplication_error(float a, float b, float x) {
       return multiplication_error(a,b,x,4097); //pow(2,mantissalength/2) + 1;
+
     }
 
     static double multiplication_error(double a, double b, double x) {
       return multiplication_error(a,b,x,67108865); //pow(2,mantissalength/2) + 1;
+
     }
 
     static T division_error(T a, T b, T x) {
       return (x*b - a - multiplication_error(x, b, x*b)) / b;
+    }
+
+    static T sqrt_error(T a, T x) {
+      return (x*x - a + multiplication_error(x, x, x*x)) / (((T)2.0)*x);
     }
 
   public:
@@ -51,25 +61,35 @@ class freal{
     T corrected_value() const { return this->val-this->err; }
     operator T() const { return corrected_value(); }
 
-    void operator+=(freal<T> rhs) {
+    freal<T>& operator=(const freal<T> &g1){
+      this->val = g1.val;
+      this->err = g1.err;
+      return *this;
+    }
+    freal<T>& operator=(const T &g1){
+      this->val = g1;
+      this->err = (T)0;
+      return *this;
+    }
+    void operator+=(const freal<T> rhs) {
       T value = this->val + rhs.val;
       T localerror = addition_error(this->val,rhs.val,value);
       this->val = value;
       this->err += rhs.err + localerror;
     }
-    void operator-=(freal<T> rhs) {
+    void operator-=(const freal<T> rhs) {
       T value = this->val - rhs.val;
       T localerror = addition_error(this->val,-rhs.val,value);
       this->val = value;
       this->err -= rhs.err + localerror;
     }
-    void operator*=(freal<T> rhs) {
+    void operator*=(const freal<T> rhs) {
       T value = this->val * rhs.val;
       T localerr = multiplication_error(this->val,rhs.val,value);
       this->err = this->val*rhs.err+this->err*rhs.val + localerr;
       this->val = value;
     }
-    void operator/=(freal<T> rhs) {
+    void operator/=(const freal<T> rhs) {
       T recip = 1.0 / rhs.val;
       T newval = this->val * recip;
       T localerr = division_error(this->val,rhs.val,newval);
@@ -107,6 +127,8 @@ class freal{
   template <typename T1>
   friend freal<T1> cos(const freal<T1> &);
   template <typename T1>
+  friend freal<T1> sqrt(const freal<T1> &);
+  template <typename T1>
   friend std::ostream& operator<<(std::ostream&, const freal<T1>&);
 };
 
@@ -142,7 +164,7 @@ freal<T> operator*(const freal<T> &g1,const freal<T> &g2){
 template<typename T>
 freal<T> operator/(const freal<T> &g1,const freal<T> &g2){
    T recip,newval;
-   recip = 1.0 / g2.val;
+   recip = (T)1.0 / g2.val;
    newval = g1.val * recip;
    T localerr = freal<T>::division_error(g1.val,g2.val,newval);
    return freal<T>(newval,recip*g1.err - recip*newval*g2.err + localerr);
@@ -194,6 +216,15 @@ freal<T> fabs(const freal<T> &g1){
 }
 
 template<typename T>
+freal<T> sqrt(const freal<T> &g1){
+   T newval,two;
+   two = 2.0;
+   newval = sqrt(g1.val);
+   T localerr = freal<T>::sqrt_error(g1.val,newval);
+   return freal<T>(newval,g1.err/(two*newval) + localerr);
+}
+
+template<typename T>
 std::ostream& operator<<(std::ostream &ost, const freal<T> &ad){
   ost << "[" << ad.value() << ", " << ad.error() << ", " << (ad.corrected_value()) << "]";
   return ost;
@@ -201,3 +232,5 @@ std::ostream& operator<<(std::ostream &ost, const freal<T> &ad){
 
 #pragma omp declare reduction(+ : freal<float> : freal<float>::ompReduce(omp_out, omp_in))
 #pragma omp declare reduction(+ : freal<double> : freal<double>::ompReduce(omp_out, omp_in)) 
+
+#endif

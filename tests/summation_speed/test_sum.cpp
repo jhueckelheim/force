641f023071985f70a10be4435c61c43278495e35
jan_hu <jhueckelheim@anl.gov>
2020-12-19 03:04:31 +0000

    bug fixes and better output in summation_speed experiment

--- tests/summation_speed/test_sum.cpp
+++ tests/summation_speed/test_sum.cpp
@@ -4,6 +4,7 @@
 #include "quadhelper.hpp"
 #include <random>
 #include <chrono>
+#include <omp.h>
 
 template<typename T>
 class KahanReal {
@@ -22,16 +23,18 @@ class KahanReal {
 #pragma omp declare reduction(+ : KahanReal<long double> : KahanReal<long double>::ompAdd(omp_out, omp_in))
 
 template<typename T>
-T benchmark(double* dval, int n, double& time) {
+T benchmark(double* dval, int n, int rep, double& time) {
   auto val = new T[n];
   for(int i=0; i<n; i++) {
     val[i] = (T)dval[i];
   }
   auto t_start = std::chrono::high_resolution_clock::now();
   T sum = 0;
-  #pragma omp parallel for reduction(+:sum)
-  for(int i=0; i<n; i++) {
-    sum += val[i];
+  for(int r=0; r<rep; r++) {
+    #pragma omp parallel for reduction(+:sum)
+    for(int i=0; i<n; i++) {
+      sum += val[i];
+    }
   }
   auto t_end = std::chrono::high_resolution_clock::now();
   time = std::chrono::duration<double, std::milli>(t_end-t_start).count();
@@ -40,19 +43,21 @@ T benchmark(double* dval, int n, double& time) {
 }
 
 template<typename T>
-T benchmark_kahan(double* dval, int n, double& time) {
+T benchmark_kahan(double* dval, int n, int rep, double& time) {
   auto val = new T[n];
   for(int i=0; i<n; i++) {
     val[i] = (T)dval[i];
   }
   auto t_start = std::chrono::high_resolution_clock::now();
   KahanReal<T> sum;
-  #pragma omp parallel for reduction(+:sum)
-  for(int i=0; i<n; i++) {
-    T y = val[i] - sum.cor;
-    T t = sum.sum + y;
-    sum.cor = (t - sum.sum) - y;
-    sum.sum = t;
+  for(int r=0; r<rep; r++) {
+    #pragma omp parallel for reduction(+:sum)
+    for(int i=0; i<n; i++) {
+      T y = val[i] - sum.cor;
+      T t = sum.sum + y;
+      sum.cor = (t - sum.sum) - y;
+      sum.sum = t;
+    }
   }
   auto t_end = std::chrono::high_resolution_clock::now();
   time = std::chrono::duration<double, std::milli>(t_end-t_start).count();
@@ -62,12 +67,14 @@ T benchmark_kahan(double* dval, int n, double& time) {
 
 int main(int argc, char** argv) {
   // Read command line arguments and set up random input data
-  if(argc < 3) {
-    std::cout<<"Usage: exec_test_sum <numvals> <distribution>"<<std::endl;
+  if(argc < 4) {
+    std::cout<<"Usage: exec_test_sum <numvals> <reps> <distribution>"<<std::endl;
     exit(-1);
   }
+  std::cout<<"NTHREADS: "<<omp_get_max_threads()<<std::endl;
   int n = atoi(argv[1]);
-  float dist = atof(argv[2]);
+  int rep = atoi(argv[2]);
+  float dist = atof(argv[3]);
   auto dval = new double[n];
   std::default_random_engine generator;
   std::exponential_distribution<double> distribution(dist);
@@ -80,58 +87,58 @@ int main(int argc, char** argv) {
 
   // Warmup to make timings consistent. The first run is usually slow.
   {
-    float sum = benchmark<float>(dval, n, elapsed_time);
+    float sum = benchmark<float>(dval, n, rep, elapsed_time);
     std::cout<<"warmup "<<sum<<" time "<<elapsed_time<<std::endl;
   }
 
   // Benchmark single precision
   {
-    float sum = benchmark<float>(dval, n, elapsed_time);
+    float sum = benchmark<float>(dval, n, rep, elapsed_time);
     std::cout<<"single_sum "<<sum<<" time "<<elapsed_time<<std::endl;
   }
   // Benchmark double precision
   {
-    double sum = benchmark<double>(dval, n, elapsed_time);
+    double sum = benchmark<double>(dval, n, rep, elapsed_time);
     std::cout<<"double_sum "<<sum<<" time "<<elapsed_time<<std::endl;
   }
   // Benchmark long double precision
   {
-    long double sum = benchmark<long double>(dval, n, elapsed_time);
+    long double sum = benchmark<long double> (dval, n, rep, elapsed_time);
     std::cout<<"longdouble_sum "<<sum<<" time "<<elapsed_time<<std::endl;
   }
   // Benchmark quad precision
   {
-    __float128 sum = benchmark<__float128>(dval, n, elapsed_time);
+    __float128 sum = benchmark<__float128>(dval, n, rep, elapsed_time);
     std::cout<<"quad_sum "<<sum<<" time "<<elapsed_time<<std::endl;
   }
   // Benchmark single precision with CENA
   {
-    freal<float> sum = benchmark<freal<float>>(dval, n, elapsed_time);
+    freal<float> sum = benchmark<freal<float>>(dval, n, rep, elapsed_time);
     std::cout<<"csingle_sum "<<sum<<" time "<<elapsed_time<<std::endl;
   }
   // Benchmark double precision with CENA
   {
-    freal<double> sum = benchmark<freal<double>>(dval, n, elapsed_time);
+    freal<double> sum = benchmark<freal<double>>(dval, n, rep, elapsed_time);
     std::cout<<"cdouble_sum "<<sum<<" time "<<elapsed_time<<std::endl;
   }
   // Benchmark long double precision with CENA
   {
-    freal<long double> sum = benchmark<freal<long double>>(dval, n, elapsed_time);
+    freal<long double> sum = benchmark<freal<long double>>(dval, n, rep, elapsed_time);
     std::cout<<"clongdouble_sum "<<sum<<" time "<<elapsed_time<<std::endl;
   }
   // Benchmark single precision with Kahan
   {
-    float sum = benchmark_kahan<float>(dval, n, elapsed_time);
+    float sum = benchmark_kahan<float>(dval, n, rep, elapsed_time);
     std::cout<<"ksingle_sum "<<sum<<" time "<<elapsed_time<<std::endl;
   }
   // Benchmark double precision with Kahan
   {
-    double sum = benchmark_kahan<double>(dval, n, elapsed_time);
+    double sum = benchmark_kahan<double>(dval, n, rep, elapsed_time);
     std::cout<<"kdouble_sum "<<sum<<" time "<<elapsed_time<<std::endl;
   }
   // Benchmark long double precision with Kahan
   {
-    long double sum = benchmark_kahan<long double>(dval, n, elapsed_time);
+    long double sum = benchmark_kahan<long double> (dval, n, rep, elapsed_time);
     std::cout<<"klongdouble_sum "<<sum<<" time "<<elapsed_time<<std::endl;
   }
 
